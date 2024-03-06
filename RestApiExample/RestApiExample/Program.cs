4ae3f20b0ef6c53d1fc3f35f33d51bdb0ba2077f
BiM4Bush <151048@student.uwm.edu.pl>
2024-03-06 16:10:53 +0100

    Setup Db connection

--- RestApiExample/RestApiExample/Program.cs
+++ RestApiExample/RestApiExample/Program.cs
@@ -1,3 +1,5 @@
+using RestApiExample.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<InventoryContext>(options =>
+    options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDbContext<PriceContext>(options =>
+    options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDbContext<ProductContext>(options =>
+    options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
